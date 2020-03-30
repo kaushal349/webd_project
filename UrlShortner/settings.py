2f6c8c52f9fe06ed46f91ee3ee85acbd5f379123
kaushal349 <kaushalaggarwal349@gmail.com>
2020-03-30 18:40:17 +0530

    cc

--- UrlShortner/settings.py
+++ UrlShortner/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e9*87!80nhk7we!ix!n@4dgme%r!u+^t4s(90v2ks!usbp)y*e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
